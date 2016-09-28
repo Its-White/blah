96e00614d4c445c47ba1dfb8282dd481280c0114
Alex Armstrong <shuffleberry@gmail.com>
2016-09-28 15:11:52 +1000

    blahh

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager{
+
+    public int playerCount;
+
+    void Start()
+    {
+        if (playerCount < 2)
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
